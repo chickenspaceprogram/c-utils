53278b8908225c317ee8210dc24234ab3d1a8dea
Athena Boose <pestpestthechicken@yahoo.com>
2025-05-20 19:47:22 -0700

    Changed over to MPL license, added some code.

--- include/c-utils/mem.h
+++ include/c-utils/mem.h
@@ -1,49 +1,66 @@
 
-// Copyright 2024, 2025 Athena Boose
-
-// This file is part of c-utils.
-
-// c-utils is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-
-// c-utils is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with c-utils. If not, see <https://www.gnu.org/licenses/>. 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #pragma once
 
 #include <stddef.h>
 
 struct cu_allocator {
+	// Required, setting alloc = NULL is undefined behavior
 	void *(*alloc)(size_t amount, void *ctx); // May return NULL if a failure occurs
+
+	// Nullable
+	
+	// If free == NULL, free will be a no-op.
 	void (*free)(void *mem, size_t amount, void *ctx); // Should no-op on NULL `mem`
-	void *(*realloc)(void *mem, size_t newsize, size_t oldsize, void *ctx); // nullable, if NULL a combination of alloc and free will be used instead
+	// If realloc == NULL, a manual alloc, memcpy, free will take place.
+	void *(*realloc)(void *mem, size_t newsize, size_t oldsize, void *ctx);
 	void *ctx; // passed into all functions
 };
 
-
-// generic functions 
-
-void *cu_allocator_realloc(void *mem, size_t newsize, size_t oldsize, const struct cu_allocator *alloc);
-
-void *cu_allocator_reallocarray(void *mem, size_t new_nel, size_t old_nel, size_t elem_size, const struct cu_allocator *alloc);
+struct cu_arena_elem;
 
 struct cu_arena {
-	struct arena_elem *first;
+	// Required
+	struct cu_arena_elem *first;
 	size_t default_block_size;
-	struct cu_allocator *allocator; // If this is NULL, standard allocator facilities will be used.
+	
+	// Nullable (malloc will get used instead)
+	const struct cu_allocator *allocator;
 };
 
-// Creates a new arena with a requested block size.
-void cu_arena_new(struct cu_arena *arena, size_t block_size);
+// generic functions, these default to libc functions when alloc == NULL
 
+void *cu_allocator_alloc(size_t memsize, const struct cu_allocator *alloc);
+void cu_allocator_free(void *mem, size_t memsize, const struct cu_allocator *alloc);
+void *cu_allocator_realloc(void *mem, size_t newsize, size_t oldsize, const struct cu_allocator *alloc);
+// same as freebsd reallocf api, frees the pointer if reallocation fails
+inline static void *cu_allocator_reallocf(void *mem, size_t newsize, size_t oldsize, const struct cu_allocator *alloc)
+{
+	void *result = cu_allocator_realloc(mem, newsize, oldsize, alloc);
+	if (result == NULL) {
+		cu_allocator_free(mem, oldsize, alloc);
+		return NULL;
+	}
+	return result;
+}
+void *cu_allocator_reallocarray(void *mem, size_t new_nel, size_t old_nel, size_t elem_size, const struct cu_allocator *alloc);
+
+// This function is inefficient if you're doing `realloc` often.
+// If you need to `realloc` an arena is a bad datastructure.
+//
+// However, if you are just allocating things, this allows for genericity.
+void cu_arena_to_allocator(struct cu_allocator *allocator, struct cu_arena *arena);
+
+// Arena functions
+inline static void cu_arena_new(struct cu_arena *arena, size_t block_size, const struct cu_allocator *alloc)
+{
+	arena->first = NULL;
+	arena->default_block_size = block_size;
+	arena->allocator = alloc;
+}
 void *cu_arena_alloc(struct cu_arena *arena, size_t item_size);
-
 void cu_arena_free(struct cu_arena *arena);
 
--- src/mem.c
+++ src/mem.c
@@ -1,46 +1,56 @@
-
-// Copyright 2024, 2025 Athena Boose
-
-// This file is part of c-utils.
-
-// c-utils is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-
-// c-utils is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with c-utils. If not, see <https://www.gnu.org/licenses/>. 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <c-utils/mem.h>
 
-struct arena_elem {
-	struct arena_elem *next;
+struct cu_arena_elem {
+	struct cu_arena_elem *next;
 	size_t block_size; // the block starts at &arena + sizeof(arena)
 	size_t amount_filled;
 };
 
+void *cu_allocator_alloc(size_t memsize, const struct cu_allocator *alloc)
+{
+	if (alloc == NULL)
+		return malloc(memsize);
+
+	return alloc->alloc(memsize, alloc->ctx);
+}
+
+void cu_allocator_free(void *mem, size_t memsize, const struct cu_allocator *alloc)
+{
+	if (alloc == NULL) {
+		free(mem);
+		return;
+	}
+	
+	if (alloc->free != NULL)
+		alloc->free(mem, memsize, alloc->ctx);
+}
+
+
 void *cu_allocator_realloc(void *mem, size_t newsize, size_t oldsize, const struct cu_allocator *alloc)
 {
+	if (alloc == NULL)
+		return realloc(mem, newsize);
+	
 	if (alloc->realloc == NULL) {
 		void *newbuf = alloc->alloc(newsize, alloc->ctx);
 		if (newbuf == NULL) {
 			return NULL;
 		}
 		memcpy(newbuf, mem, oldsize);
-		alloc->free(mem, oldsize, alloc->ctx);
+		cu_allocator_free(mem, oldsize, alloc);
+		return newbuf;
 	}
 	return alloc->realloc(mem, newsize, oldsize, alloc->ctx);
 }
 
-bool check_mult_overflow(size_t n1, size_t n2)
+static bool check_mult_overflow(size_t n1, size_t n2)
 {
 	// should in theory work
 	size_t div1 = SIZE_MAX / n1;
@@ -58,60 +68,75 @@ void *cu_allocator_reallocarray(void *mem, size_t new_nel, size_t old_nel, size_
 	return cu_allocator_realloc(mem, new_nel * elem_size, old_nel * elem_size, alloc);
 }
 
-
-
-size_t cu_arena_new_size(size_t block_size)
+static void *cu_arena_allocator_fn(size_t amount, void *ctx)
 {
-	return sizeof(struct cu_arena) + block_size;
+	struct cu_arena *arena = ctx;
+	return cu_arena_alloc(arena, amount);
 }
 
-static struct arena_elem *find_allocatable_elem(struct arena_elem *first, size_t item_size)
+void cu_arena_to_allocator(struct cu_allocator *allocator, struct cu_arena *arena)
 {
-	for (struct arena_elem *elem = first; elem != NULL; elem = elem->next) {
-		if (item_size < (elem->block_size - elem->amount_filled)) {
-			return elem;
-		}
+	allocator->alloc = cu_arena_allocator_fn;
+	allocator->free = NULL;
+	allocator->realloc = NULL;
+	allocator->ctx = arena;
+}
+
+static void *get_valid_space(const struct cu_arena_elem *el, size_t index)
+{
+	return (char *)el + sizeof(const struct cu_arena_elem) + index;
+}
+
+static struct cu_arena_elem *new_cu_arena_elem(size_t block_size, const struct cu_allocator *allocator)
+{
+	size_t alloc_size = block_size + sizeof(struct cu_arena_elem);
+	struct cu_arena_elem *new_elem = NULL;
+	if (allocator == NULL)
+		new_elem = malloc(alloc_size);
+	else
+		new_elem = cu_allocator_alloc(alloc_size, allocator);
+
+	if (new_elem != NULL) {
+		new_elem->next = NULL;
+		new_elem->block_size = block_size;
+		new_elem->amount_filled = 0;
 	}
-	return NULL;
+	return new_elem;
 }
 
-// returns a pointer to the data space
-static void *setup_new_node(struct cu_arena *arena, void *buffer, size_t item_size)
-{
-		size_t alloc_size = (item_size > arena->default_block_size) ? item_size : arena->default_block_size; 
-		struct arena_elem new_node = {
-			.next = arena->first,
-			.block_size = alloc_size,
-			.amount_filled = item_size,
-		};
-		memcpy(buffer, &new_node, sizeof(struct arena_elem));
-		arena->first = buffer;
-		return (char *)buffer + sizeof(struct arena_elem);
-	
-}
-
-void cu_arena_new(struct cu_arena *arena, size_t block_size)
-{
-	arena->first = NULL;
-	arena->default_block_size = block_size;
-}
 
 void *cu_arena_alloc(struct cu_arena *arena, size_t item_size)
 {
-	size_t alloc_size = cu_arena_alloc_size(arena, item_size);
-	void *buffer = NULL;
-	if (alloc_size != 0) {
-		buffer = malloc(alloc_size);
-		if (buffer == NULL) {
+	if (arena->first == NULL) {
+		size_t block_size = (item_size > arena->default_block_size) ? item_size : arena->default_block_size;
+		arena->first = new_cu_arena_elem(block_size, arena->allocator);
+		if (arena->first == NULL)
 			return NULL;
-		}
+
+		arena->first->amount_filled = item_size;
+		return get_valid_space(arena->first, 0);
 	}
-	return cu_arena_alloc_buf(arena, item_size, buffer);
+	struct cu_arena_elem *target_elem = arena->first;
+	struct cu_arena_elem *last_elem = NULL;
+	while (target_elem != NULL && (target_elem->block_size - target_elem->amount_filled) < item_size) {
+		last_elem = target_elem;
+		target_elem = target_elem->next;
+	}
+	if (target_elem == NULL) {
+		size_t block_size = (item_size > arena->default_block_size) ? item_size : arena->default_block_size;
+		last_elem->next = new_cu_arena_elem(block_size, arena->allocator);
+		last_elem->next->amount_filled = item_size;
+		return get_valid_space(last_elem->next, 0);
+	}
+	void *space = get_valid_space(target_elem, target_elem->amount_filled);
+	target_elem->amount_filled += item_size;
+	return space;
 }
 
 void cu_arena_free(struct cu_arena *arena)
 {
-	for (void *freeable = cu_arena_free_buf(arena); freeable != NULL; freeable = cu_arena_free_buf(arena)) {
-		free(freeable);
+	for (struct cu_arena_elem *el = arena->first; el != NULL; el = el->next) {
+		cu_allocator_free(el, el->block_size + sizeof(struct cu_arena_elem), arena->allocator);
 	}
 }
+
