71558f50c754a1b89b83ff1275abb90d8bd3e33c
Athena Boose <pestpestthechicken@yahoo.com>
2025-07-07 20:29:24 -0700

    savestate

--- src/include/cu/sync.h
+++ src/include/cu/sync.h
@@ -1,9 +1,17 @@
 #pragma once
 
-#ifdef CUTILS_HAVE_C11_THREADS
+typedef struct cu_sem_internal cu_sem;
+
+#if defined(CUTILS_HAVE_C11_THREADS) && ASDFAFDASFSDAS
 #include <threads.h>
 #elif defined(CUTILS_HAVE_PTHREADS)
 // define c11 threads in terms of pthreads
+
+#ifdef __STDC_NO_THREADS__
+#undef __STDC_NO_THREADS__
+#endif
+
+#include <pthread.h>
 enum {
     thrd_success = 0,
     thrd_nomem = -2,
@@ -11,39 +19,76 @@ enum {
     thrd_busy = -4,
     thrd_error = -1
 };
+
+typedef int (*thrd_start_t)(void *);
+typedef pthread_mutex_t mtx_t;
+typedef pthread_cond_t cnd_t;
+typedef pthread_t thrd_t;
+
+enum {
+	mtx_plain = 0x1,
+	mtx_timed = 0x2,
+	mtx_recursive = 0x4,
+};
+
+
+typedef pthread_once_t once_flag;
+#define ONCE_FLAG_INIT PTHREAD_ONCE_INIT
+
+
+typedef pthread_key_t tss_t;
+#define TSS_DTOR_ITERATIONS PTHREAD_DESTRUCTOR_ITERATIONS
+typedef void (*tss_dtor_t)(void *);
+
+
+int thrd_create(thrd_t *thr, thrd_start_t func, void *arg);
+int thrd_equal(thrd_t lhs, thrd_t rhs);
+thrd_t thrd_current(void);
+int thrd_sleep(const struct timespec *duration, struct timespec *remaining);
+void thrd_yield(void);
+[[noreturn]] void thrd_exit(int res);
+int thrd_detach(thrd_t thr);
+int thrd_join(thrd_t thr, int *res);
+
+int mtx_init(mtx_t *mutex, int type);
+int mtx_lock(mtx_t *mutex);
+int mtx_timedlock(
+	mtx_t *restrict mutex,
+	const struct timespec *restrict time_point
+);
+int mtx_trylock(mtx_t *mutex);
+int mtx_unlock(mtx_t *mutex);
+void mtx_destroy(mtx_t *mutex);
+
+void call_once(once_flag *flag, void (*func)(void));
+
+int cnd_init(cnd_t *cond);
+int cnd_signal(cnd_t *cond);
+int cnd_broadcast(cnd_t *cond);
+int cnd_wait(cnd_t *cond, mtx_t *mutex);
+int cnd_timedwait(
+	cnd_t* restrict cond,
+	mtx_t* restrict mutex,
+	const struct timespec *restrict time_point
+);
+void cnd_destroy(cnd_t *cond);
+
+int tss_create(tss_t *tss_key, tss_dtor_t destructor);
+void *tss_get(tss_t tss_key);
+int tss_set(tss_t tss_id, void *val);
+void tss_delete(tss_t tss_id);
+
 #else
 #error "c-utils requires either C11 threads or pthreads to compile"
 #endif
 
 // presume c11 threading stuff is defined
-#ifdef CUTILS_HAVE_PTHREADS // prefer pthreads over my ugly implementation
-#include <semaphore.h>
-typedef sem_t cu_sem;
-static inline int cu_sem_init(cu_sem *sem, unsigned int init_value)
-{
-	int retval = sem_init(sem, 0, init_value);
-	if (retval == 0)
-		return thrd_success;
-	else
-		return thrd_error;
-}
-int cu_sem_post(cu_sem *sem)
-{
-	if (sem_post(sem) == 0)
-		return thrd_success;
-	else
-		return thrd_error;
-}
-int cu_sem_try_wait(cu_sem *sem);
-int cu_sem_timedwait(cu_sem *sem, const struct timespec *restrict time);
-int cu_sem_wait(cu_sem *sem);
-void cu_sem_destroy(cu_sem *sem);
-#else
-typedef struct {
-	unsigned int counter; // unsigned int to correspond with POSIX semaphores
+
+struct cu_sem_internal {
 	cnd_t cond;
 	mtx_t mutex;
-} cu_sem;
+	unsigned int counter; // unsigned int to correspond with POSIX semaphores, in case using those is more efficient
+};
 
 static inline int cu_sem_init(cu_sem *sem, unsigned int init_value)
 {
@@ -62,4 +107,3 @@ static inline void cu_sem_destroy(cu_sem *sem)
 	cnd_destroy(&(sem->cond));
 	mtx_destroy(&(sem->mutex));
 }
-#endif
--- src/sync.c
+++ src/sync.c
@@ -1,60 +1,169 @@
 #include <cu/sync.h>
-#define MAX_EINTR_COUNT 128
 
-#ifdef CUTILS_HAVE_PTHREADS
-#include <errno.h>
+#if 1// !defined(CUTILS_HAVE_C11_THREADS) && defined(CUTILS_HAVE_PTHREADS)
 #include <assert.h>
-int cu_sem_try_wait(cu_sem *sem)
+#include <stdint.h>
+
+#include <pthread.h>
+#include <time.h>
+static_assert(sizeof(intptr_t) >= sizeof(int), "intptr_t should be bigger than int");
+
+struct cbinfo {
+	thrd_start_t func;
+	void *arg;
+	cu_sem blocker;
+};
+
+static void *pthread_dummy_cb(void *arg)
 {
-	int retval = 0;
-	int eintr_ct = MAX_EINTR_COUNT;
-	do {
-		retval = sem_trywait(sem);
-		if (retval == 0)
-			return thrd_success;
-		else if (retval == EAGAIN)
-			return thrd_busy;
-	} while (retval == EINTR && eintr_ct-- > 0);
-	return thrd_error;
-}
-int cu_sem_timedwait(cu_sem *sem, const struct timespec *restrict time)
-{
-	int retval = 0;
-	int eintr_ct = MAX_EINTR_COUNT;
-	do {
-		retval = sem_timedwait(sem, time);
-		if (retval == 0)
-			return thrd_success;
-		else if (retval == ETIMEDOUT)
-			return thrd_busy;
-	} while (retval == EINTR && eintr_ct-- > 0);
-	return thrd_error;
-}
-int cu_sem_wait(cu_sem *sem)
-{
-	int retval = 0;
-	int eintr_ct = MAX_EINTR_COUNT;
-	do {
-		retval = sem_wait(sem);
-		if (retval == 0)
-			return thrd_success;
-	} while (retval == EINTR && eintr_ct-- > 0);
-	return thrd_error;
+	struct cbinfo *info = arg;
+
+	void *fnarg = info->arg;
+	thrd_start_t fn = info->func;
+
+	cu_sem_post(&(info->blocker)); // gotten everything we need, now post the semaphore and move on
+
+	intptr_t retval = (intptr_t)fn(fnarg);
+	return (void *)retval;
 }
 
 
-void cu_sem_destroy(cu_sem *sem)
+int thrd_create(thrd_t *thr, thrd_start_t func, void *arg)
 {
-	int res = sem_destroy(sem);
-	assert(res == 0);
+	struct cbinfo info = {
+		.func = func,
+		.arg = arg,
+	};
+	int retval = cu_sem_init(&(info.blocker), 0);
+	if (retval != 0)
+		return thrd_error;
+
+	retval = pthread_create(thr, NULL, pthread_dummy_cb, &info);
+	if (retval != 0)
+		return thrd_error;
+	retval = cu_sem_wait(&(info.blocker));
+	if (retval != thrd_success)
+		return retval;
+	
+	cu_sem_destroy(&(info.blocker));
+
+	return thrd_error;
 }
-#else
+
+int thrd_equal(thrd_t lhs, thrd_t rhs)
+{
+	return pthread_equal(lhs, rhs);
+}
+thrd_t thrd_current(void)
+{
+	return pthread_self();
+}
+int thrd_sleep(const struct timespec *duration, struct timespec *remaining)
+{
+	return nanosleep(duration, remaining);
+}
+void thrd_yield(void)
+{
+	sched_yield();
+}
+[[noreturn]] void thrd_exit(int res)
+{
+	intptr_t resptr = (intptr_t)res;
+	pthread_exit((void *)resptr);
+}
+int thrd_detach(thrd_t thr)
+{
+	if (pthread_detach(thr) == 0)
+		return thrd_success;
+	return thrd_error;
+}
+int thrd_join(thrd_t thr, int *res)
+{
+	void *retval;
+	if (pthread_join(thr, &retval) != 0)
+		return thrd_error;
+	intptr_t intretval = (intptr_t)retval;
+	return intretval;
+}
+
+typedef pthread_mutex_t mtx_t;
+
+int mtx_init(mtx_t *mutex, int type)
+{
+	if ((type & mtx_plain) && (type & mtx_timed))
+		return thrd_error;
+	if (!(type & mtx_plain) && !(type & mtx_timed))
+		return thrd_error;
+	
+	if (!(type & mtx_recursive)) {
+		pthread_mutex_t new_mutex = PTHREAD_MUTEX_INITIALIZER;
+		*mutex = new_mutex;
+		return thrd_success;
+	}
+	
+	pthread_mutexattr_t attr;
+	if (pthread_mutexattr_init(&attr) != 0)
+		return thrd_error;
+	if (pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE) != 0)
+		return thrd_error;
+	
+	int retval = pthread_mutex_init(mutex, &attr);
+
+	if (pthread_mutexattr_destroy(&attr) != 0)
+		return thrd_error;
+	if (retval != 0)
+		return thrd_error;
+	return thrd_success;
+}
+
+int mtx_lock(mtx_t *mutex)
+{
+	if (pthread_mutex_lock(mutex) != 0)
+		return thrd_error;
+	return thrd_success;
+}
+int mtx_timedlock(
+	mtx_t *restrict mutex,
+	const struct timespec *restrict time_point
+)
+{
+	if (pthread_mutex_timedlock(mutex, time_point) != 0)
+		return thrd_error;
+	return thrd_success;
+}
+int mtx_trylock(mtx_t *mutex)
+{
+	
+}
+int mtx_unlock(mtx_t *mutex);
+void mtx_destroy(mtx_t *mutex);
+
+void call_once(once_flag *flag, void (*func)(void));
+
+int cnd_init(cnd_t *cond);
+int cnd_signal(cnd_t *cond);
+int cnd_broadcast(cnd_t *cond);
+int cnd_wait(cnd_t *cond, mtx_t *mutex);
+int cnd_timedwait(
+	cnd_t* restrict cond,
+	mtx_t* restrict mutex,
+	const struct timespec *restrict time_point
+);
+void cnd_destroy(cnd_t *cond);
+int tss_create(tss_t *tss_key, tss_dtor_t destructor);
+void *tss_get(tss_t tss_key);
+int tss_set(tss_t tss_id, void *val);
+void tss_delete(tss_t tss_id);
+
+
+#endif
+
 int cu_sem_post(cu_sem *sem)
 {
 	int retval = mtx_lock(&(sem->mutex));
-	++(sem->counter);
 	if (retval != thrd_success)
 		return retval;
+	++(sem->counter);
 	
 	retval = cnd_signal(&(sem->cond));
 	int unlock_res = mtx_unlock(&(sem->mutex));
@@ -114,4 +223,3 @@ int cu_sem_timedwait(cu_sem *sem, const struct timespec *restrict time)
 		return res;
 	return thrd_success;
 }
-#endif
