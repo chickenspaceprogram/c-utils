962aaa67b71b5cd417ef13f568bbb71c1768fb4c
Athena Boose <pestpestthechicken@yahoo.com>
2025-07-09 00:32:25 -0700

    Revamped arena

--- src/arena.c
+++ src/arena.c
@@ -1,63 +1,66 @@
 #include <cu/arena.h>
+#include <stdint.h>
+#include <assert.h>
 
-#if !__has_c_attribute(gnu::may_alias)
-#error "cu_arena requires gnu::may_alias attribute"
-#endif
-
+#define IS_PWR_2(V) ((V) && !((V) & ((V) - 1)))
 
 struct cu_arena {
 	size_t bufsize;
 	size_t index;
-	[[gnu::may_alias]] max_align_t buf[];
+	alignas(alignof(max_align_t)) uint8_t buf[];
 };
 struct cu_arena_dyn {
 	struct cu_arena_elem *first;
 	struct cu_allocator *alloc;
 	size_t default_block_size;
 	size_t index;
-	[[gnu::may_alias]] max_align_t buf[];
+	alignas(alignof(max_align_t)) uint8_t buf[];
 };
 struct cu_arena_elem {
 	size_t bufsize;
 	size_t index;
 	struct cu_arena_elem *next;
-	[[gnu::may_alias]] max_align_t buf[];
+	alignas(alignof(max_align_t)) uint8_t buf[];
 };
 
+static_assert(alignof(struct cu_arena) <= alignof(max_align_t), "Your platform aligns structs weirdly.");
+static_assert(alignof(struct cu_arena_dyn) <= alignof(max_align_t), "Your platform aligns structs weirdly.");
+static_assert(alignof(struct cu_arena_elem) <= alignof(max_align_t), "Your platform aligns structs weirdly.");
 
-static size_t get_index_increment(size_t nbytes)
+static size_t align_index(uintptr_t baseptr, size_t cur_index, size_t align)
 {
-	size_t mod = nbytes % sizeof(max_align_t);
-	if (mod != 0)
-		mod = 1;
-	return nbytes / sizeof(max_align_t) + mod;
+	assert(IS_PWR_2(align) && "Alignment must be a power of 2");
+	size_t mod = (baseptr + cur_index) & (align - 1);
+	size_t extra = align - mod;
+	assert(((baseptr + cur_index + extra) & (align - 1)) == 0 && "Something is misaligned...");
+	return cur_index + extra;
 }
-
 
 struct cu_arena *cu_arena_new(size_t arena_size, struct cu_allocator *alloc)
 {
-	struct cu_arena *arena = cu_allocator_alloc(sizeof(struct cu_arena) + get_index_increment(arena_size) * sizeof(max_align_t), alloc);
+	struct cu_arena *arena = cu_allocator_alloc(sizeof(struct cu_arena) + arena_size, alloc);
 	if (arena == NULL)
 		return NULL;
-	arena->bufsize = get_index_increment(arena_size);
+	arena->bufsize = arena_size;
 	arena->index = 0;
 	return arena;
 }
 
-void *cu_arena_alloc(size_t amt, struct cu_arena *arena)
+void *cu_arena_aligned_alloc(size_t amt, size_t align, struct cu_arena *arena)
 {
-	size_t index_inc = get_index_increment(amt);
-	if (index_inc + arena->index > arena->bufsize) {
+	assert(IS_PWR_2(align));
+	size_t base_index = align_index((uintptr_t)arena->buf, arena->index, align);
+	if (base_index + amt > arena->bufsize) {
 		return NULL;
 	}
-	void *elem = arena->buf + arena->index;
-	arena->index += index_inc;
+	void *elem = arena->buf + base_index;
+	arena->index = base_index + amt;
 	return elem;
 }
 
 void cu_arena_free(struct cu_arena *arena, struct cu_allocator *alloc)
 {
-	cu_allocator_free(alloc, arena->bufsize * sizeof(max_align_t) + sizeof(struct cu_arena), alloc);
+	cu_allocator_free(alloc, arena->bufsize + sizeof(struct cu_arena), alloc);
 }
 
 static void *arena_allocator_alloc(size_t amount, void *ctx)
@@ -76,41 +79,65 @@ void cu_arena_cast(struct cu_allocator *alloc, struct cu_arena *arena)
 
 struct cu_arena_dyn *cu_arena_dyn_new(size_t block_size, struct cu_allocator *alloc)
 {
-	struct cu_arena_dyn *arena = cu_allocator_alloc(sizeof(struct cu_arena_dyn) + get_index_increment(block_size) * sizeof(max_align_t), alloc);
+	if (block_size < alignof(max_align_t))
+		block_size = alignof(max_align_t);
+	struct cu_arena_dyn *arena = cu_allocator_alloc(sizeof(struct cu_arena_dyn) + block_size, alloc);
 	if (arena == NULL)
 		return NULL;
-	arena->default_block_size = get_index_increment(block_size);
+	arena->default_block_size = block_size;
 	arena->index = 0;
 	arena->first = NULL;
 	arena->alloc = alloc;
 	return arena;
 }
-void *cu_arena_dyn_alloc(size_t amt, struct cu_arena_dyn *arena)
+
+static size_t max_reqd_size(size_t amt, size_t align)
 {
-	size_t alloc_size = get_index_increment(amt);
-	if (arena->index + alloc_size <= arena->default_block_size) {
-		void *ptr = arena->buf + arena->index;
-		arena->index += alloc_size;
-		return ptr;
+	if (align <= alignof(max_align_t))
+		return amt;
+	return amt + align - alignof(max_align_t);
+}
+void *cu_arena_dyn_aligned_alloc(size_t amt, size_t align, struct cu_arena_dyn *arena)
+{
+	size_t reqd_size = max_reqd_size(amt, align);
+	if (reqd_size > arena->default_block_size) {
+		struct cu_arena_elem *new_block = cu_allocator_alloc(reqd_size + sizeof(struct cu_arena_elem), arena->alloc);
+		if (new_block == NULL)
+			return NULL;
+
+		size_t start_index = align_index((uintptr_t)new_block->buf, 0, align);
+
+		new_block->index = start_index + amt;
+		new_block->bufsize = reqd_size;
+		new_block->next = arena->first;
+		arena->first = new_block;
+		return new_block->buf + start_index;
 	}
+	size_t base_index = align_index((uintptr_t)arena->buf, arena->index, align);
+	if (base_index + amt <= arena->default_block_size) {
+		arena->index = base_index + amt;
+		return arena->buf + base_index;
+	}
+
 	struct cu_arena_elem *elem = arena->first;
 	while (elem != NULL) {
-		if (elem->index + alloc_size <= elem->bufsize) {
-			void *ptr = elem->buf + elem->index;
-			elem->index += alloc_size;
-			return ptr;
+		base_index = align_index((uintptr_t)elem->buf, elem->index, align);
+		if (base_index + amt <= elem->bufsize) {
+			elem->index = base_index + amt;
+			return elem->buf + base_index;
 		}
 		elem = elem->next;
 	}
-	size_t new_bufsize = alloc_size > arena->default_block_size ? alloc_size : arena->default_block_size;
-	struct cu_arena_elem *new_first = cu_allocator_alloc(sizeof(struct cu_arena_elem) + new_bufsize, arena->alloc);
+	struct cu_arena_elem *new_first = cu_allocator_alloc(sizeof(struct cu_arena_elem) + arena->default_block_size, arena->alloc);
 	if (new_first == NULL)
 		return NULL;
-	new_first->bufsize = new_bufsize;
-	new_first->index = alloc_size;
+	new_first->bufsize = arena->default_block_size;
+	new_first->index = align_index((uintptr_t)elem->buf, 0, align);
+	void *retval = new_first->buf + new_first->index;
+	new_first->index += amt;
 	new_first->next = arena->first;
 	arena->first = new_first;
-	return new_first->buf;
+	return retval;
 }
 void cu_arena_dyn_free(struct cu_arena_dyn *arena)
 {
@@ -118,9 +145,9 @@ void cu_arena_dyn_free(struct cu_arena_dyn *arena)
 	while (elem != NULL) {
 		struct cu_arena_elem *tmp = elem;
 		elem = elem->next;
-		cu_allocator_free(tmp, sizeof(struct cu_arena_elem) + tmp->bufsize * sizeof(max_align_t), arena->alloc);
+		cu_allocator_free(tmp, sizeof(struct cu_arena_elem) + tmp->bufsize, arena->alloc);
 	}
-	cu_allocator_free(arena, sizeof(struct cu_arena_dyn) + sizeof(max_align_t) * arena->default_block_size, arena->alloc);
+	cu_allocator_free(arena, sizeof(struct cu_arena_dyn) + arena->default_block_size, arena->alloc);
 }
 
 static void *arena_dyn_alloc(size_t amount, void *ctx)
--- src/include/cu/arena.h
+++ src/include/cu/arena.h
@@ -1,12 +1,35 @@
 #pragma once
 #include <stddef.h>
+#include <stdalign.h>
 #include <cu/alloc.h>
 
 struct cu_arena;
 struct cu_arena_dyn;
 
 struct cu_arena *cu_arena_new(size_t arena_size, struct cu_allocator *alloc);
-void *cu_arena_alloc(size_t amt, struct cu_arena *arena);
+// Allocates `amt` bytes of memory with an alignment of `align`.
+// `align` must be a power of two.
+void *cu_arena_aligned_alloc(size_t amt, size_t align, struct cu_arena *arena);
+
+// Only valid when allocating space for types with "fundamental alignment" as
+// defined by the C standard! (malloc also has this limitation)
+//
+// These are:
+// - basic types
+// - enumerated types
+// - pointer types
+// - arrays of types w/ fundamental alignment
+// - structures/unions whose component types have fundamental alignment
+//
+// If a type doesn't have fundamental alignment (for example, you used _Alignas
+// on it), use cu_arena_aligned_alloc.
+//
+// cu_arena_aligned_alloc is also beneficial if you need to allocate multiple
+// types with small alignments consecutive to each other.
+static inline void *cu_arena_alloc(size_t amt, struct cu_arena *arena)
+{
+	return cu_arena_aligned_alloc(amt, alignof(max_align_t), arena);
+}
 void cu_arena_free(struct cu_arena *arena, struct cu_allocator *alloc);
 
 void cu_arena_cast(struct cu_allocator *alloc, struct cu_arena *arena);
@@ -14,7 +37,11 @@ void cu_arena_cast(struct cu_allocator *alloc, struct cu_arena *arena);
 
 
 struct cu_arena_dyn *cu_arena_dyn_new(size_t arena_size, struct cu_allocator *alloc);
-void *cu_arena_dyn_alloc(size_t amt, struct cu_arena_dyn *arena);
+void *cu_arena_dyn_aligned_alloc(size_t amt, size_t align, struct cu_arena_dyn *arena);
+static inline void *cu_arena_dyn_alloc(size_t amt, struct cu_arena_dyn *arena)
+{
+	return cu_arena_dyn_aligned_alloc(amt, alignof(max_align_t), arena);
+}
 void cu_arena_dyn_free(struct cu_arena_dyn *arena);
 
 void cu_arena_dyn_cast(struct cu_allocator *alloc, struct cu_arena_dyn *arena);
