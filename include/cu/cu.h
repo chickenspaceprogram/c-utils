0f0c7e723201e3b422b944285fd29400d75bb484
Athena Boose <pestpestthechicken@yahoo.com>
2025-12-28 13:42:43 -0800

    Added cu.h file to include the whole lib

---
+++ include/cu/cu.h
@@ -0,0 +1,19 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// SPDX-License-Identifier: MPL-2.0
+
+
+// if you're lazy and want to include the entire lib, include this
+
+#pragma once
+
+#include <cu/alloc.h>
+#include <cu/arena.h>
+#include <cu/bitmanip.h>
+#include <cu/dbgassert.h>
+#include <cu/hashmap.h>
+#include <cu/rand.h>
+#include <cu/siphash.h>
+#include <cu/string.h>
